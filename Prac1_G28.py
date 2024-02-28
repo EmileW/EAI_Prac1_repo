39ee32f67fa78cb79615949c8372b7aa4ab8755a
EmileW <u21494780@tuks.co.za>
2024-02-28 14:23:29 +0200

    Emile_DFS Final
    DFS and Tree complete

--- Prac1_G28.py
+++ Prac1_G28.py
@@ -16,7 +16,7 @@ Notes for Self: Object - Rock, Paper or Scissor
 import numpy as np
 
 
-bfs_dfs = 0 # Variable to change between Depth First Search and 
+bfs_dfs = 1 # Variable to change between Depth First Search and 
             # Breath First Seach tree traveral algorithms. bfs_dfs = 0 (BFS) bfs_dfs = 1 (DFS)
 
 # Node class for the search tree
@@ -25,22 +25,23 @@ class Node:
         self.rpsobject = rpsobject #The Rock, Paper or Scissor represented by the Node
         self.children = [None, None, None]  # Each node has 3 children as there could be 3 different Objects that could be played after 
                                             # the Object stored in the node
+        self.visited = 0
 
 # Recursive Tree constructor, with 3 children for each node (R, P and S).
 # The depth of the tree is input depth-1
-def construct_search_tree(depth):
+def construct_search_tree(depth,rpsobject = ""):
     if depth == 0:
         return None
-    root = Node("") #Root Node has no Object
+    root = Node(rpsobject) #Root Node has no Object
     if depth > 1:
-        root.children[0] = construct_search_tree(depth - 1) # Child 1
-        root.children[0].rpsobject = "R"                    # Rock
+        root.children[0] = construct_search_tree(depth - 1,root.rpsobject +"R") # Child 1
+        root.children[0].rpsobject = root.rpsobject + "R"                    # Rock
         
-        root.children[1] = construct_search_tree(depth - 1) # Child 2
-        root.children[1].rpsobject = "P"                    # Paper
+        root.children[1] = construct_search_tree(depth - 1,root.rpsobject +"P") # Child 2
+        root.children[1].rpsobject = root.rpsobject+ "P"                    # Paper
             
-        root.children[2] = construct_search_tree(depth - 1) # Child 3
-        root.children[2].rpsobject = "S"                    # Scissor
+        root.children[2] = construct_search_tree(depth - 1,root.rpsobject +"S") # Child 3
+        root.children[2].rpsobject = root.rpsobject + "S"                    # Scissor
     return root
 
 def print_tree(root, depth=0):
@@ -48,19 +49,114 @@ def print_tree(root, depth=0):
         print("  " * depth + str(root.rpsobject))
         for child in root.children:
             print_tree(child, depth + 1)
-
-
-# root = construct_search_tree(10)
-# print_tree(root)
-
-if not input:
-    # This is the first Move
-    Tree_Size = 2
+            
+            
+def DFS(root):
+    if root is None:
+        return []
     
+    stack = [root]  # Stack to keep track of nodes to visit
+    visited = []    # List to store visited node values
+    while stack:
+        current_node = stack.pop()  # Pop the top node from the stack
+        current_node.visited = 1
+        if not current_node == root:
+            # Store the value of the current node
+            visited.append(current_node.rpsobject)
+        
+        # Push the children of the current node onto the stack in reverse order
+        # This ensures that the leftmost child is visited first
+        for child in reversed(current_node.children):
+            if child is not None:
+                stack.append(child)            
     
+    return visited
+
+def test_for_break(prev_input):
+    if (input == prev_input):
+        return True
+    else:
+        return False
+    
+def win_move(input):
+    if input == "R":
+        return "P"
+    
+    if input == "P":
+        return "S"
+    
+    if input == "S":
+        return "R"
+
+
+if  input == "":
+    
+    Tree_Depth = 2
+    root = construct_search_tree(Tree_Depth)
+    i = 0
+    dfs_list = DFS(root)
+    seq = dfs_list[i]
+    i = i + 1
+    output = seq[0]
+    seq = seq[1:]
+    last_input = input
+    break_sequence = ""
+    false_break = 0
+    # print(output)
+    
+        
 else:
-    # This is the 2nd+ Move
-    print("AWE")
+    if (len(seq) == 0):
+        # All of the Objects for the node have been played, a new node is needed.
+        # Test if a break sequence occured
+        if test_for_break(last_input):
+            # The previous sequence was a break sequence !
+            false_break = 0
+            break_sequence = dfs_list[i-1]
+            
+            output = win_move(input)
+        else:
+            # no break sequence
+            if (len(break_sequence) > 0 ):
+                seq = break_sequence
+                output = seq[0]
+                seq = seq[1:]
+                false_break = false_break + 1
+                if (false_break > 3):
+                    # We found a false break sequence
+                    break_sequence = ""
+            else:  
+                # First test if there are still nodes to be tested
+                if (i < len(dfs_list)):
+                    # Move to the next node
+                    seq = dfs_list[i]
+                    i = i + 1
+                    output = seq[0]
+                    seq = seq[1:]
+                else:
+                    # All of the nodes have been tested, create a larger search tree
+                    Tree_Depth = Tree_Depth +1
+                    root = construct_search_tree(Tree_Depth)
+                    i = 0
+                    dfs_list = DFS(root)
+                    seq = dfs_list[i]
+                    i = i + 1
+                    output = seq[0]
+                    seq = seq[1:]
+            
+    else:
+        # There are still objects in the node to be played
+        output = seq[0]
+        seq = seq[1:]        
+    last_input = input
+    
+    # print(output)
+    
+    
+    
+   
+            
+
     
     
 
